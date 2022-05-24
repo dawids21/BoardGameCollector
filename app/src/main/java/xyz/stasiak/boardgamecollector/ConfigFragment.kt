dea11b621c89fd3388a850d1ed0a3b69bab8e26f
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-05-24 17:37:24 +0200

    save name set in config fragment

--- app/src/main/java/xyz/stasiak/boardgamecollector/ConfigFragment.kt
+++ app/src/main/java/xyz/stasiak/boardgamecollector/ConfigFragment.kt
@@ -1,5 +1,6 @@
 package xyz.stasiak.boardgamecollector
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,8 +12,9 @@ import xyz.stasiak.boardgamecollector.databinding.FragmentConfigBinding
 class ConfigFragment : Fragment() {
 
     private var _binding: FragmentConfigBinding? = null
-
     private val binding get() = _binding!!
+    private var _userNameDbHandler: UserNameDbHandler? = null
+    private val userNameDbHandler get() = _userNameDbHandler!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +22,7 @@ class ConfigFragment : Fragment() {
     ): View {
 
         _binding = FragmentConfigBinding.inflate(inflater, container, false)
+        binding.configName.setText(userNameDbHandler.getName()?.name)
         return binding.root
 
     }
@@ -28,8 +31,14 @@ class ConfigFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.configBtnStart.setOnClickListener {
+            userNameDbHandler.saveName(UserName(binding.configName.text.toString()))
             findNavController().navigate(R.id.action_ConfigFragment_to_MainFragment)
         }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        _userNameDbHandler = UserNameDbHandler(context, null)
     }
 
     override fun onDestroyView() {
