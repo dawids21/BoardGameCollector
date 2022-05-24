1dea9e3c57f29b9c359c68aba85db3c17f9d795b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-05-24 19:16:12 +0200

    create Game class

---
+++ app/src/main/java/xyz/stasiak/boardgamecollector/Game.kt
@@ -0,0 +1,11 @@
+package xyz.stasiak.boardgamecollector
+
+class Game(
+    val id: Long,
+    val title: String,
+    val originalTitle: String,
+    val year: Int,
+    val bggId: Long,
+    val rank: Int
+) {
+}
