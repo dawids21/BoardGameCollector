0236371330a7557c9c2417904fc2c16f9ecda849
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-05-26 14:54:04 +0200

    create class for async images downloader

---
+++ app/src/main/java/xyz/stasiak/boardgamecollector/ImagesDownloaderAsync.kt
@@ -0,0 +1,34 @@
+@file:Suppress("DEPRECATION")
+
+package xyz.stasiak.boardgamecollector
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
+import android.widget.ImageView
+import java.net.URL
+
+@SuppressLint("StaticFieldLeak")
+class ImagesDownloaderAsync(private val imageView: ImageView, private val imageUrl: URL) :
+    AsyncTask<String, Void, Bitmap>() {
+
+    override fun doInBackground(vararg p0: String?): Bitmap? {
+        var bitmap: Bitmap? = null
+        try {
+            bitmap = BitmapFactory.decodeStream(
+                imageUrl.openConnection().getInputStream()
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return bitmap
+    }
+
+    override fun onPostExecute(result: Bitmap?) {
+        super.onPostExecute(result)
+        if (result != null) {
+            imageView.setImageBitmap(result)
+        }
+    }
+}
